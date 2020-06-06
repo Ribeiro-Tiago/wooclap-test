c5a7ec4e69bcc867ac74ed4e5c0641f29e6f40ec
Tiago Ribeiro <tiago@accountable.com>
2020-06-06 20:30:18 +0200

    app: added functionality & styling to searchbar

--- app/src/components/SearchBar/SearchBar.tsx
+++ app/src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./SearchBar.scss";
+import debounce from "../../utils/debounce";
+import { searchMovies } from "../../services/api";
 
 export default function SearchBar() {
+  const [isFetching, setFetching] = useState<boolean>(false);
+  let input: HTMLInputElement;
+
+  const onTriggerSearch = async () => {
+    if (isFetching) {
+      return;
+    }
+
+    setFetching(true);
+    await searchMovies(input.value).finally(() => setFetching(false));
+  };
+
+  const onChange = () => debounce(onTriggerSearch);
+
   return (
     <div className="searchbar">
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={onChange}
+        ref={(ref) => {
+          if (ref) {
+            input = ref;
+          }
+        }}
+      />
 
-      <button>Search</button>
+      <button onClick={onTriggerSearch}>Search</button>
     </div>
   );
 }
