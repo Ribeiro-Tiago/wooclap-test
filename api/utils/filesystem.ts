d50a7d7d6ea9b60d2723a6490506e07c5ce0b0bd
Tiago Ribeiro <tiago@accountable.com>
2020-06-07 17:14:09 +0200

    api: added function to save uploaded file

--- api/utils/filesystem.ts
+++ api/utils/filesystem.ts
@@ -1,10 +1,31 @@
-import { rmdirSync, existsSync } from "fs";
-import { join } from "path";
+import {
+  mkdirSync,
+  existsSync,
+  writeFileSync,
+  readFileSync,
+  unlinkSync,
+} from "fs";
+import { join, resolve } from "path";
+
+const publicDir = resolve(__dirname, "../", process.env.PUBLIC_FILES_DIR);
 
 export const removePublicFile = (name: string) => {
-  const path = join("../", process.env.PUBLIC_FILES_DIR, name);
+  const path = join(publicDir, name);
 
   if (existsSync(path)) {
-    return rmdirSync(path);
+    return unlinkSync(path);
   }
+};
+
+// not really sure what's the type for file
+export const uploadPublicFile = (tmpFilePath: string, filename: string) => {
+  const path = join(publicDir, filename);
+
+  if (!existsSync(publicDir)) {
+    mkdirSync(publicDir);
+  }
+
+  writeFileSync(path, readFileSync(tmpFilePath));
+
+  return path;
 };
