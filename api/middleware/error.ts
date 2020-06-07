dba745f3eb2e6f0e13fa3e11aaf8cdb5795795a7
Tiago Ribeiro <tiago@accountable.com>
2020-06-07 17:17:35 +0200

    api: updated error middleware to accept extra details

--- api/middleware/error.ts
+++ api/middleware/error.ts
@@ -1,9 +1,20 @@
 import { CustomError, Request, Response, Next } from "../types/server";
 
 export const handleError = async (req: Request, res: Response, next: Next) => {
-  res.error = (err: CustomError) => {
+  res.error = (err: CustomError, errors?: Object) => {
     res.statusCode = err.status || 500;
-    res.json(err);
+
+    if (!errors) {
+      return res.json(errors);
+    }
+
+    const formattedErrors = Object.entries(errors).reduce((accu, curr) => {
+      accu.push({ key: curr[0], message: curr[1] });
+
+      return accu;
+    }, []);
+
+    res.json({ ...err, errors: formattedErrors });
   };
 
   next();
