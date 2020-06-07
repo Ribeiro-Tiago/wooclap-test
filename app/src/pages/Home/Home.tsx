a30c92cf8de17249f089075aea226f80e3079bf2
Tiago Ribeiro <tiago@accountable.com>
2020-06-07 14:57:30 +0200

    app: added add button to hoem page

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+import "./Home.scss";
 import { SearchBar, MovieList } from "../../components";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="home">
-      <h1>Movie Catalog</h1>
+      <header>
+        <h1>Movie Catalog</h1>
+        <Link to="/details/-1">
+          <span>Create</span>
+        </Link>
+      </header>
 
       <SearchBar />
 
