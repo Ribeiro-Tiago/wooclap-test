c7ef49acc748d6618702e4570ace441d6926d9bb
Tiago Ribeiro <tiago@accountable.com>
2020-06-08 17:01:52 +0200

    app: added a loader ©omponent

--- app/src/Navigator.tsx
+++ app/src/Navigator.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import { MovieDetails, Home } from "./pages";
+import { Loader } from "./components";
 
 export default function Navigator() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/details/:id" component={MovieDetails} />
+    <>
+      <Loader />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/details/:id" component={MovieDetails} />
 
-        <Route path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
